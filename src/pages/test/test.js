33df697874815134ce10d9333baa69660ecef9e4
liujunchina <asdf123456>
2016-11-07 18:15:14 +0800

    es6 学习

--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -2,6 +2,69 @@
  * Created by Liu.Jun on 2016/10/11.
  */
 
-let [a,b,c] = ['43',41234,'fasdf'];
+var result = {
+    errCode : 0,
+    errMsg :'我是错误提示',
+    obj:{
+        obj:'fasdf'
+    }
+}
 
-console.log(a);
+const [a,b,c] = 'fasdfasfd';
+console.log(a,b,c);
+
+let {toString:s} = 123;
+console.log(s === Number.prototype.toString);
+
+// 函数的解构赋值
+function test([x,y]) {
+    console.log(x+y)
+}
+
+var testArr = [[1,312],[234,43]].map(([a,b])=> {
+    return a + b;
+});
+
+function move({x = 1, y = 2} = {}) {
+    console.log(x + 'move To' + y);
+}
+move({
+    x:3,
+    y:4
+})
+move()
+
+///
+
+var map = new Map();
+map.set('f','1');
+map.set('s','2');
+console.log(map[0]);
+
+for(let [key,value] of map){
+    console.log(key + '   ' + value);
+}
+
+// unicode
+
+var name = '柳俊';
+
+for(let s of name){
+    console.log(s);
+}
+
+console.log('abc'.charAt(0)) // "a"
+console.log('𠮷'.charAt(0)) // "\uD842"
+
+//
+var strData = {
+    name:'liu',
+    age:'1008'
+}
+var str = `fsad\`fasd${strData.liu}
+
+
+fdasdf,
+姓名：${strData.age}`;
+
+console.log(str);
