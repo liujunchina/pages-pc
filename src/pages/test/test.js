9a097db7aa866ec67d6c810e38951905a7b90ee9
Liu.Jun <870395192@qq.com>
2016-12-04 12:47:04 +0800

    压缩代码  webpack -> gulp

--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -1,15 +1,31 @@
 /**
  * Created by Liu.Jun on 2016/10/11.
  */
-var p1 = new Promise((resolve,reject)=>{
-    setTimeout(()=> {
-        reject(new Error('fail'));
-    }, 3000);
-});
 
-var p2 = new Promise((resolve,reject)=>{
-    setTimeout(()=>resolve(p1),1000)
-});
+// let [a,b,c] = ['43',41234,'fasdf'];
+//
+// console.log(a);
 
-p2.then(result=>console.log(result))
-    .catch(error=>console.log(error));
+let testDom = document.getElementById('j_test');
+// let p = document.createElement('p');
+// let text = document.createTextNode('我是js创建的 ');
+// let testBefore = document.createElement('div');
+// testBefore.innerText = '我是创建在之前的元素';
+// p.appendChild(text);
+// console.log(p.nodeType);
+// testDom.appendChild(p);     // 插入p标签
+//
+// testDom.insertBefore(testBefore,p); // p之前插入div
+
+let xr = new XMLHttpRequest();
+xr.open('GET','/res.txt',true);
+xr.onreadystatechange = function () {
+    if(xr.readyState == 4){
+        let innerText = document.createTextNode(xr.responseText);
+        testDom.appendChild(innerText);
+    }
+}
+xr.send(null);
+
+
+
